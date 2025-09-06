e023df109f288a217b9f617b344b9982016ce285
SENCOINSN <165965655+SENCOINSN@users.noreply.github.com>
2025-09-06 18:43:59 +0100

    Update request necessary field

--- src/main/java/com/sid/gl/elections/bulletins/BulletinRequestDto.java
+++ src/main/java/com/sid/gl/elections/bulletins/BulletinRequestDto.java
@@ -4,8 +4,7 @@ import lombok.Data;
 
 @Data
 public class BulletinRequestDto {
-    private String lastName;
-    private String firstName;
-    private String partyName;
+   private String email //email user candidat
+   private String nameElection // nom de l'election
 
 }
