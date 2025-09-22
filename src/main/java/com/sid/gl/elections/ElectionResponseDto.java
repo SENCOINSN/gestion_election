834e0aed3586c336d6db63f8b9862c238263d1e7
SENCOINSN <165965655+SENCOINSN@users.noreply.github.com>
2025-09-22 18:29:21 +0100

    rm validation on response

--- src/main/java/com/sid/gl/elections/ElectionResponseDto.java
+++ src/main/java/com/sid/gl/elections/ElectionResponseDto.java
@@ -7,11 +7,7 @@ import java.time.LocalDateTime;
 
 public record ElectionResponseDto(
         Long id,
-        @NotNull( message = "name must not be null")
-        @NotBlank(message = "name is required")
         String name,
-
-        @NotNull(message = "desc must not be null")
         String description,
         LocalDateTime dateStart
 ) {
